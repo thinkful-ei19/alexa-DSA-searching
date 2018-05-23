1101179bfd35f6421af7730eaffb47aea74e280e
Alexa Scott <arscott4792@gmail.com>
2018-05-23 08:55:19 -0700

    setup form for linear search

--- src/components/linear.js
+++ src/components/linear.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default class Linear extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+
+        }
+      }
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    let linearSearchInputValue = e.target.linearSearch.value;
+                    e.target.linearSearch.value = '';
+                    console.log(linearSearchInputValue);
+                }}>
                     <label>Linear Search</label> <br />
-                    <input type="text" name="linear-search"></input>
+                    <input type="text" name="linearSearch"></input>
                     <button type="submit">Search</button>
                 </form>
             </div>
