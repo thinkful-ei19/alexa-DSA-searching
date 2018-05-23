9f57ce43be1cbcbdab1440edb936912280620186
Alexa Scott <arscott4792@gmail.com>
2018-05-23 12:31:49 -0700

    created drills file and completed find a book

---
+++ playground/drills.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Find a book
+
+// sorted library array:
+
+// const library = [
+//     { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+//     { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+//     { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+//     { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+//     { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+//     { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+//     { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+//     { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+//     { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+//     { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+//   ];
+
+// using the Dewey Decimal system
+// as long as books in library are sorted, you can use a binary searching algorithm to find book.
+// first, start with finding midpoint, compare the Dewey Decimal value of book we want with midpoint
+// is this desired value/target greater than or smaller than the midpoint?
+// can eliminate that side the target is not on and repeat until target = item at that index
+
+
